fed3f7ca3ec60145bc0b462a3bf577a29f99d0a2
Rookez <54031130+Rookez@users.noreply.github.com>
2019-10-01 17:26:40 +0200

    api request started

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,12 +1,13 @@
 const openLigaDB = 'https://www.openligadb.de/api/getmatchdata/bl1/2019';
 
-function UpdateTeam () {
-  if (team === "team1") {
-    document.getElementById('')
-  }
-  else if (team === "team2") {
-    document.getElementById('')
-  }
+function updateResult () {
+  axios
+  .get(openLigaDB)
+  .then(responseFromAPI => {
+    const matchData = responseFromAPI.data;
+    let myMatch = matchData.filter(match => match.Team1.TeamName == "FC Bayern" && match.Team2.TeamName == "Hertha BSC")
+    console.log(myMatch)
+  })
 }
 
 
